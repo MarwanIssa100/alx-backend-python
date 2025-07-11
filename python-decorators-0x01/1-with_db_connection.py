3bbcf3e72cb6c4874b27edbd7f771992cc2ab10c
MarwanIssa100 <missa4312@gmail.com>
2025-07-11 18:20:44 +0300

    add decorator task 2

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -19,3 +19,25 @@ def get_user_by_id(conn, user_id):
 
 user = get_user_by_id(user_id=1)
 print(user)
+
+#### decorator to handle transactions
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        conn.execute('BEGIN')
+        result = func(conn, *args, **kwargs)
+        conn.commit()
+        conn.close()
+        return result
+    return wrapper
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+    #### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
