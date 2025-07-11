2a3594d2984b372b5b3de906e44c1f3cb368df06
MarwanIssa100 <missa4312@gmail.com>
2025-07-11 19:28:33 +0300

    add async task 2

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,21 +1,20 @@
 import asyncio
 import aiosqlite
 
-async def async_fetch_users(db_path):
+tsks = [async def async_fetch_users(db_path):
     async with aiosqlite.connect(db_path) as conn:
         async with conn.cursor() as cursor:
             await cursor.execute("SELECT * FROM users")
             return await cursor.fetchall()
-        
+        ,
 async def async_fetch_older_users(db_path):
     async with aiosqlite.connect(db_path) as conn:
         async with conn.cursor() as cursor:
             await cursor.execute("SELECT * FROM users WHERE age > ?",(40,)) 
             return await cursor.fetchall()
-        
+]
 async def fetch_concurrently():
-    tasks = [async_fetch_users('users.db'), async_fetch_older_users('users.db')]
-    results = await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tsks)
     print(results)
 
 asyncio.run(fetch_concurrently())
