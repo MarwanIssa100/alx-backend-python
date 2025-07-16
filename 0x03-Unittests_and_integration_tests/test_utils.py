3b477fd12310b99b0ce2d431c86dc52c1943d3c6
MarwanIssa100 <missa4312@gmail.com>
2025-07-16 22:31:07 +0300

    edit in task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,16 @@
 from utils import access_nested_map
-from pytest import raises
+import unittest
 
-def test_access_nested_map():
-    nested_map = {"a": {"b": {"c": 1}}}
-    assert access_nested_map(nested_map, ["a", "b", "c"]) == 1
-    assert access_nested_map(nested_map, ["a", "b"]) == {"c": 1}
+class TestAccessNestedMap(unittest.TestCase):
     
-def test_access_nested_map_missing_key():
-    nested_map = {"a": {"b": {"c": 1}}}
-    with raises(KeyError):
-        access_nested_map(nested_map, ["a", "b", "d"])
-    
+    def test_access_nested_map(self):
+        nested_map = {"a": {"b": {"c": 42}}}
+        self.assertEqual(access_nested_map(nested_map, ("a", "b", "c")), 42)
+        
+    def test_access_nested_map_missing_key(self):
+        nested_map = {"a": {"b": {"c": 42}}}
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, ("a", "b", "d"))
+
+if __name__ == '__main__':
+    unittest.main()    
