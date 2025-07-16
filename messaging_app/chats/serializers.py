54256d6c5d9ed51a1fd93349e34f14ea43b01574
MarwanIssa100 <missa4312@gmail.com>
2025-07-17 01:03:57 +0300

    add user, conversation and message models

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,5 +1,7 @@
 from .models import Conversation, Message, user
 from rest_framework import serializers
+from rest_framework.serializers import CharField, SerializerMethodField
+from rest_framework.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ['user_id']
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_body = CharField(max_length=1000)
+    
     class Meta:
         model = Message
         fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
@@ -15,9 +19,25 @@ class MessageSerializer(serializers.ModelSerializer):
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    message = MessageSerializer(many=True, read_only=True)
+    message = SerializerMethodField()
+    
+    def get_message(self, obj):
+        messages = obj.messages.all()
+        return MessageSerializer(messages, many=True).data
+    
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'created_at', 'participants', 'message']
         read_only_fields = ['conversation_id', 'created_at']
+        
+    def create(self, validated_data):
+        participants_data = validated_data.pop('participants')
+        conversation = Conversation.objects.create(**validated_data)
+        for participant in participants_data:
+            try:
+                user_instance = user.objects.get(user_id=participant.user_id)
+                conversation.participants.add(user_instance)
+            except user.DoesNotExist:
+                raise ValidationError(f"User with ID {participant.user_id} does not exist")
+        return conversation
         
