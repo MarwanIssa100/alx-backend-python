ec217575da76292d5c886d80a8904bc2a495bc86
MarwanIssa100 <missa4312@gmail.com>
2025-07-17 00:55:17 +0300

    add user, conversation and message models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
 
 class user(AbstractUser):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    primary_key = True
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     
