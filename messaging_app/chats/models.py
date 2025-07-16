57d257900661b6a25c150c96cf4fa0b447e78c10
MarwanIssa100 <missa4312@gmail.com>
2025-07-17 00:56:46 +0300

    add user, conversation and message models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -18,18 +18,19 @@ class user(AbstractUser):
     def __str__(self):
         return self.username
     
-class conversation(models.Model):
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     participants = models.ManyToManyField(user, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"Conversation {self.id} with {self.participants.count()} participants"
     
-class message(models.Model):
-    conversation = models.ForeignKey(conversation, related_name='messages', on_delete=models.CASCADE)
+class Message(models.Model):
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
     sender = models.ForeignKey(user, related_name='sent_messages', on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)    
     def __str__(self):
         return f"Message from {self.sender.username} in Conversation {self.conversation.id} at {self.timestamp}"
