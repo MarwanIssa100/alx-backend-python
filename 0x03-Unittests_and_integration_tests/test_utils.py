142d95dd376781a383ee2f1ba0b450e47c779d33
MarwanIssa100 <missa4312@gmail.com>
2025-07-16 22:38:42 +0300

    edit in task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ class TestAccessNestedMap(unittest.TestCase):
         
     def test_access_nested_map_missing_key(self):
         nested_map = {"a": {"b": {"c": 42}}}
-        with self.assertRaises(KeyError):
+        with self.assertRaises(KeyError, msg="KeyError should be raised"):
             access_nested_map(nested_map, ("a", "b", "d"))
 
 if __name__ == '__main__':
