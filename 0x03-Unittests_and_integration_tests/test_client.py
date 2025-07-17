bc70308b03e8c1985368b87f26954ac3a0c056e6
MarwanIssa100 <missa4312@gmail.com>
2025-07-17 23:31:03 +0300

    add task 5 code to test_client file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,6 +8,7 @@ from parameterized import parameterized
 from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
+    
     """
     Unit tests for the GithubOrgClient class.
     """
@@ -17,8 +18,10 @@ class TestGithubOrgClient(unittest.TestCase):
         ("microsoft", {"login": "microsoft", "id": 2}),
     ])
     def test_org(self, org_name, expected):
+        
         """
-        Test that the org method returns the correct organization data.
+        Test that the org method 
+        returns the correct organization data.
         """
         with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
             mock_org.return_value = expected
