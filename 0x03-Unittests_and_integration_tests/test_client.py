3e08c9d1d1be5d8fb9aadc527f0b090fe677deff
MarwanIssa100 <missa4312@gmail.com>
2025-07-17 23:29:42 +0300

    add task 5 code to test_client file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,6 @@ from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
-
 class TestGithubOrgClient(unittest.TestCase):
     """
     Unit tests for the GithubOrgClient class.
@@ -32,7 +31,6 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         org_name = "test_org"
         expected_url = "https://api.github.com/orgs/test_org/repos"
-        
         client = GithubOrgClient(org_name)
         self.assertEqual(client._public_repos_url, expected_url)
 
@@ -50,15 +48,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = test_payload
         test_url = "http://test_url"
         org_name = "test_org"
-        with patch.object(GithubOrgClient, '_public_repos_url', 
-                        new_callable=PropertyMock) as mock_url:
+        with patch.object(
+            GithubOrgClient,
+            '_public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_url:
             mock_url.return_value = test_url
             client = GithubOrgClient(org_name)
             repos = client.public_repos()
             self.assertEqual(repos, ["repo1", "repo2", "repo3"])
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with(test_url)
-
 
 if __name__ == '__main__':
     unittest.main()
