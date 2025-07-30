02a1247bf83e9bce9e83948215b14339abb99103
MarwanIssa100 <missa4312@gmail.com>
2025-07-31 01:23:28 +0300

    edit  managers.py

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class UnreadMessageManager(models.Manager):
+class UnreadMessagesManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(read=False)
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response 
 from django.contrib.auth.models import User
 from .models import Message 
-from .managers import UnreadMessageManager
+from .managers import UnreadMessagesManager
 
 # Create your views here.
 
@@ -51,7 +51,7 @@ def get_unread_messages(request):
     """
     View to get all unread messages for the authenticated user.
     """
-    unread_messages = UnreadMessageManager().get_queryset().filter(receiver=request.user).only('content', 'sender')
+    unread_messages = UnreadMessagesManager().get_queryset().filter(receiver=request.user).only('content', 'sender')
     serialized_unread_messages = []
     
     for message in unread_messages:
