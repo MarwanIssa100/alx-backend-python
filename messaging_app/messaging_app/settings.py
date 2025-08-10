b312a38af1fd7722fc30ca614711b4a3fb8d2c59
MarwanIssa100 <missa4312@gmail.com>
2025-08-11 01:15:50 +0300

    ok

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'messaging_app',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
